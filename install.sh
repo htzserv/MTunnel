af77241838d83424e9f8c3b5054524bd16007629
LuncLine <126993694+htzserv@users.noreply.github.com>
2026-07-08 19:33:08 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-# --- MHDesign Master Core | Auto Installer ---
+# --- MDesign Master Core | Auto Installer v1.2 ---
 
 B='\033[1;34m'; G='\033[1;32m'; Y='\033[1;33m'; DIM='\033[2;37m'; NC='\033[0m'
 REPO="https://raw.githubusercontent.com/htzserv/MTunnel/main"
 
 clear
 echo -e "\n  ${B}╭────────────────────────────────────────╮${NC}"
-echo -e "  ${B}│${NC} ${W}MHDesign Core Initializing...${NC}          ${B}│${NC}"
+echo -e "  ${B}│${NC} ${W}MDesign Core Initializing...${NC}           ${B}│${NC}"
 echo -e "  ${B}╰────────────────────────────────────────╯${NC}\n"
 
 echo -e "  ${DIM}├─ Creating workspace directory (MTunnel)...${NC}"
@@ -14,10 +14,10 @@ mkdir -p MTunnel
 cd MTunnel || exit
 
 echo -e "  ${DIM}├─ Fetching Core Modules...${NC}"
-# قرار دادن تایم‌اوت ۱۰ ثانیه‌ای برای دانلودها جهت جلوگیری از معلق ماندن پروسس
 curl --connect-timeout 10 -m 20 -sO "$REPO/main.sh"
 curl --connect-timeout 10 -m 20 -sO "$REPO/mgre.sh"
 curl --connect-timeout 10 -m 20 -sO "$REPO/mporter.sh"
+curl --connect-timeout 10 -m 20 -sO "$REPO/mdiag.sh"
 
 echo -e "  ${DIM}├─ Applying execution permissions...${NC}"
 chmod +x *.sh

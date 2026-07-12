6c99f965708a2edcc817328d5afacb5306ef8628
LuncLine <126993694+htzserv@users.noreply.github.com>
2026-07-12 19:43:06 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# --- MDesign Master Core | Central Installer v4.2.4 (Native Execution) ---
+# --- MDesign Master Core | Central Installer v4.2.5 (Clean Exec) ---
 
 B='\033[1;34m'; G='\033[1;32m'; Y='\033[1;33m'; R='\033[1;31m'; C='\033[0;36m'; W='\033[1;37m'; DIM='\033[2;37m'; NC='\033[0m'
 REPO_BASE="https://raw.githubusercontent.com/htzserv/MTunnel/main"
@@ -37,7 +37,6 @@ done
 
 echo -e "  ${G}● Installation Complete! Local Backup Saved.${NC}"
 echo -e "  ${DIM}└─ Launching Central Dashboard...${NC}\n"
-sleep 1
+sleep 1.5
 
-# اجرای استاندارد و تمیز داشبورد
-/usr/bin/mtunnel
+exec mtunnel

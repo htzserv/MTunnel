0fb57757e24a4de6aad4ddad1a0b8d3e4b2b293d
LuncLine <126993694+htzserv@users.noreply.github.com>
2026-09-11 04:07:43 +0330

    Update mporter.sh

--- mporter.sh
+++ mporter.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-# --- MDesign Modular Core (mporter.sh) | MPorter Manager v8.3.13 ---
+# --- MDesign Modular Core (mporter.sh) | MPorter Manager v8.3.14 ---
 # [Features: Async Background Update Checker | Stable OTA | Offline Editor]
 
-MODULE_VERSION="8.3.13"
+MODULE_VERSION="8.3.14"
 
 B='\033[1;34m'; G='\033[1;32m'; Y='\033[1;33m'; R='\033[1;31m'; W='\033[1;37m'; C='\033[0;36m'; M='\033[1;35m'; DIM='\033[2;37m'; NC='\033[0m'
 INSTALL_PATH="/usr/bin/mporter"

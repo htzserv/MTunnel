e5ff1d2b5c8ed4b1f2fe6a1a235493a7f70a4984
LuncLine <126993694+htzserv@users.noreply.github.com>
2026-09-12 01:06:04 +0330

    Update MODULE_VERSION to 2.9.12

--- tunnels/mrathole.sh
+++ tunnels/mrathole.sh
@@ -2,7 +2,7 @@
 # --- MDesign Modular Core (mrathole.sh) | The Ultimate Rathole Engine V2.9.11 ---
 # [Features: Refined Spacing | Async Background Checker | Minimal Badges]
 
-MODULE_VERSION="2.9.11"
+MODULE_VERSION="2.9.12"
 
 B='\033[1;34m'; G='\033[1;32m'; Y='\033[1;33m'; R='\033[1;31m'; C='\033[0;36m'; M='\033[1;35m'; W='\033[1;37m'; DIM='\033[2;37m'; NC='\033[0m'
 INSTALL_PATH="/usr/bin/mrathole"

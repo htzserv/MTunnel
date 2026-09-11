cf3fb4cbdfdbc438d1df6a639c06852efe684cb5
LuncLine <126993694+htzserv@users.noreply.github.com>
2026-09-12 01:56:30 +0330

    Update linktest.sh

--- tools/linktest.sh
+++ tools/linktest.sh
@@ -2,7 +2,7 @@
 # --- MDesign Modular Core (linktest.sh) | Strict Auto-Synced Benchmark & Speedtest v3.9.1 ---
 # [Features: Refined Spacing | Async Background Checker | Minimal OTA Badges]
 
-MODULE_VERSION="3.9.1"
+MODULE_VERSION="3.9.2"
 
 B='\033[1;34m'; G='\033[1;32m'; Y='\033[1;33m'; R='\033[1;31m'; C='\033[0;36m'; M='\033[1;35m'; W='\033[1;37m'; DIM='\033[2;37m'; NC='\033[0m'
 INSTALL_PATH="/usr/bin/linktest"
